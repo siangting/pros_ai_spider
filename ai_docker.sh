29a357d9b278986618bc912a0326b24296f786b7
otischung <my072814638@gmail.com>
2023-12-04 21:24:35 +0800

    [FIX] Update the newest image name

--- ai_docker.sh
+++ ai_docker.sh
@@ -1,1 +1,1 @@
-docker run -it --rm -v "$(pwd)/AI_pkg:/AI_pkg" --runtime nvidia --network host  --env-file ./.env ghcr.io/otischung/pros_ai:0.0.0 /bin/bash
+docker run -it --rm -v "$(pwd)/AI_pkg:/AI_pkg" --runtime nvidia --network host  --env-file ./.env ghcr.io/otischung/pros_ai_image:0.0.0 /bin/bash
