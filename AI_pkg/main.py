967c10de49b2a0372e7c0884e2375379e6f5e93c
p76121013 <mickey2000528@gmail.com>
2024-02-04 00:45:34 +0800

    merge two control types

--- AI_pkg/main.py
+++ AI_pkg/main.py
@@ -2,6 +2,7 @@ import rclpy
 import threading
 from ROS_receive_and_data_processing.AI_node import AI_node
 from avoidance_rule.rule_base import RuleBasedController
+from car_supervised.lstm_inference import supervised_inference
 
 def init_ros_node():
     '''node初始化並開一個thread跑ros node'''
@@ -11,21 +12,34 @@ def init_ros_node():
     thread.start()
     return node, thread
 
-def main():
+def main(mode):
     node, ros_thread = init_ros_node()
     
-    rule_controller = RuleBasedController(
+    if mode == '1':
+        rule_controller = RuleBasedController(
         node, 
         './Simulated_Annealing_model/parameters.pkl',
         load_parameters=True, 
         save_to_csv=True
-    )
+        )
+        rule_controller.run()
+    elif mode == '2':
+        supervised = supervised_inference()
+        supervised.lstm_inference(node)
+    else:
+        print("Please type the correct numbers.")
     
-    rule_controller.run()
     
     rclpy.shutdown()
     ros_thread.join()
 
 
+def print_usage():
+    print("modes:")
+    print(" 1 -- rule-based.")
+    print(" 2 -- supervised learning inference.")
+
 if __name__ == '__main__':
-    main()
+    print_usage()
+    mode = input("Enter mode: ")
+    main(mode)
