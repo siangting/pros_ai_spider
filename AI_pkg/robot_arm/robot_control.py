ce4211108293c0406effd0a150da9396c380e225
alianlbj23 <alianlbj23@gmail.com>
2024-04-08 16:28:26 +0800

    [FEAT] robot arm control

--- AI_pkg/robot_arm/robot_control.py
+++ AI_pkg/robot_arm/robot_control.py
@@ -9,14 +9,17 @@ class RobotArmControl:
     def action(self):
         # [0,0,0,0,0]
         actions = [
-            [30, 50, 100, 50, 10],
-            [130, 60, 160, 50, 10],
+            [170,50,80,50,10],
+            [170,50,0,50,10],
             # Add more actions as needed
         ]
-        for angle_set in actions:
-            radians = list(np.radians(angle_set))
-            self.node.publish_arm(radians)
-            time.sleep(1)
+        self.node.publish_arm(list(np.radians(actions[0])))
+        time.sleep(1)
+        for i in range(3):
+            for angle_set in actions:
+                radians = list(np.radians(angle_set))
+                self.node.publish_arm(radians)
+                time.sleep(0.5)
 
 def main(args=None):
     print("hello")
