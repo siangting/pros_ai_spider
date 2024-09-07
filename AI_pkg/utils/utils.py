d8dbd7fdd0165102cc95d499f286b7dc767b8fa1
roger20415 <roger20415@gmail.com>
2024-09-07 16:04:20 +0800

    [FEAT] Add convert_human_to_unity angle function.
    1. Add convert_human_to_unity_spider_joint_angles() in Unity.

--- AI_pkg/utils/utils.py
+++ AI_pkg/utils/utils.py
@@ -155,3 +155,66 @@ def two_vecs_to_angle(vec1: tuple[float, float], vec2: tuple[float, float]) -> f
     angle_degree = math.degrees(angle_radians)
     
     return angle_degree
+
+
+def convert_human_to_unity_spider_joint_angles(human_spider_angles: list[float]) -> list[float]:
+    """
+    Converts spider joint angles from human-friendly format to Unity's format.
+
+    Parameters
+    ----------
+    human_spider_angles: list[float]
+        A list of 16 angles that are intuitive for humans.
+        
+        Shoulder joints:
+            - Abduction: positive
+            - Adduction: negative
+        
+        Calf joints:
+            - Forward extension: positive
+            - Backward extension: negative
+    
+    Returns
+    ----------
+    unity_spider_angles: list[float]
+        A list of 16 angles formatted for Unity.
+        
+        Facing the spider in Unity:
+        - For the 4 front shoulder joints, positive angle values indicate counterclockwise rotation.
+        - For the 4 back shoulder joints, positive angle values indicate clockwise rotation.
+        - For the 4 left calf joints, positive angle values indicate backward extension.
+        - For the 4 right calf joints, positive angle values indicate forward extension.
+    
+    Raises
+    ----------
+    ValueError
+        If human_spider_angles does not consist of exactly 16 float values.
+    """
+
+    unity_spider_angles: list[float] = [
+        human_spider_angles[0],
+        -human_spider_angles[1],
+
+        human_spider_angles[2],
+        -human_spider_angles[3],
+
+        -human_spider_angles[4],
+        -human_spider_angles[5],
+
+        -human_spider_angles[6],
+        -human_spider_angles[7],
+
+        -human_spider_angles[8],
+        human_spider_angles[9],
+
+        -human_spider_angles[10],
+        human_spider_angles[11],
+
+        human_spider_angles[12],
+        human_spider_angles[13],
+
+        human_spider_angles[14],
+        human_spider_angles[15]
+    ]
+
+    return unity_spider_angles
