24f20fc5b03d325bf42039eb3132b2e8e1165eb8
roger20415 <roger20415@gmail.com>
2024-10-05 16:29:29 +0800

    [REFACTOR] Combined two functions into one func.
    1. Combine gym_env_register and gym_redirect_env_register to only
       gym_env_register.
    2. The gym_env_register and gym_redirect_env_register in spider-main
       must be refactored too.

--- AI_pkg/Spider_inference/inference.py
+++ AI_pkg/Spider_inference/inference.py
@@ -1,12 +1,8 @@
-from pyexpat import model
-import rclpy
-import threading
 import gymnasium as gym
 from stable_baselines3 import PPO
 from Spider_RL.RL_training_main import CustomSpiderEnv
 from Spider_redirect_RL.RL_training_redirect import CustomSpiderRedirectEnv
 from Spider_inference.inference_config import InferenceConfig
-from ros_receive_and_data_processing.AI_spider_node import AI_spider_node
 from utils import utils
 
 class Inference:
@@ -14,42 +10,26 @@ class Inference:
     _forward_model = PPO.load(InferenceConfig.FORWARD_MODEL_PATH)
     _redirect_model = PPO.load(InferenceConfig.REDIRECT_MODEL_PATH)
 
-
-    def init_AI_spider_node():
-        rclpy.init()
-        node = AI_spider_node()
-        thread = threading.Thread(target = rclpy.spin, args = (node,))
-        thread.start()
-        return node, thread
-
-    def gym_env_register(self, node):
+    def gym_env_register(self, node, env_id: str, entry_point: str):
         gym.register(
-            id = CustomSpiderEnv.ENV_NAME,  
-            entry_point = "Spider_RL.RL_training_main:CustomSpiderEnv",
+            env_id,  
+            entry_point
         )
-        return gym.make("CustomSpiderEnv-v0", AI_node = node)
-
-    def gym_redirect_env_register(self, node):
-        gym.register(
-            id = CustomSpiderRedirectEnv.ENV_NAME,  
-            entry_point = "Spider_redirect_RL.RL_training_redirect:CustomSpiderRedirectEnv",
-        )
-        return gym.make("CustomSpiderRedirectEnv-v0", AI_node = node)
+        return gym.make(env_id, AI_node = node)
     
-
     def select_model(self, angle: float, current_model) -> str:
         """
         Select the model to use based on the spider toward angle.
-        If angle < 30 degrees, use the forward model.
-        If angle >= 30 degrees, use the turning model.
+        If angle < InferenceConfig.REDIRECT_SWITCH_TO_FORWARD_ANGLE degrees, use the forward model.
+        If angle >= InferenceConfig.FORWORD_SWITCH_TO_REDIRECT_ANGLE degrees, use the turning model.
         """
-        if current_model == "forward" and abs(angle) < 30:
+        if current_model == "forward" and abs(angle) < InferenceConfig.FORWORD_SWITCH_TO_REDIRECT_ANGLE:
             next_model: str = "forward"
-        elif current_model == "forward" and abs(angle) >= 30:
+        elif current_model == "forward" and abs(angle) >= InferenceConfig.FORWORD_SWITCH_TO_REDIRECT_ANGLE:
             next_model: str = "redirect"
-        elif current_model == "redirect" and abs(angle) > 3:
+        elif current_model == "redirect" and abs(angle) > InferenceConfig.REDIRECT_SWITCH_TO_FORWARD_ANGLE:
             next_model: str = "redirect"
-        elif current_model == "redirect" and abs(angle) <= 3:
+        elif current_model == "redirect" and abs(angle) <= InferenceConfig.REDIRECT_SWITCH_TO_FORWARD_ANGLE:
             next_model: str = "forward"
         else:
             print("Choose model fail...")
@@ -63,10 +43,10 @@ class Inference:
         return angle
 
 
-    def inference(self, node):
+    def inference(self, node) -> None:
 
-        forward_env = self.gym_env_register(node)
-        redirect_env = self.gym_redirect_env_register(node)
+        forward_env = self.gym_env_register(node, CustomSpiderEnv.ENV_NAME, "Spider_RL.RL_training_main:CustomSpiderEnv")
+        redirect_env = self.gym_env_register(node, CustomSpiderRedirectEnv.ENV_NAME, "Spider_redirect_RL.RL_training_redirect:CustomSpiderRedirectEnv")
 
         redirect_env.reset()
         obs, _ = forward_env.reset()
@@ -74,10 +54,7 @@ class Inference:
 
         current_model = "forward"
         while (True):
-            # Get the robot's current angle
             spider_toward_angle = self.get_spider_toward_angle(node)
-
-            # Select the model and environment based on the angle
             current_model = self.select_model(spider_toward_angle, current_model)
 
             if current_model == "forward":
@@ -88,5 +65,4 @@ class Inference:
                 env = redirect_env
 
             action, _ = model.predict(obs)
-
-            obs, reward, done, info, _ = env.step(action)
+            obs, _, _, _, _ = env.step(action)
--- AI_pkg/Spider_inference/inference_config.py
+++ AI_pkg/Spider_inference/inference_config.py
@@ -3,3 +3,6 @@ class InferenceConfig:
 
     FORWARD_MODEL_PATH: str = "./Model/PPO_spider_2024-09-11.pt"
     REDIRECT_MODEL_PATH: str = "./redirect_Model/PPO_spider_2024-09-11.pt"
+
+    FORWORD_SWITCH_TO_REDIRECT_ANGLE: float = 30.0
+    REDIRECT_SWITCH_TO_FORWARD_ANGLE: float = 3.0
